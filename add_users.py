f4d6a633d2ee129d28e0ea9b58247b0c20d7cb2e
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-03-15 00:00:15 -0300

    :sparkles: Add the recently_seen feature

--- add_users.py
+++ add_users.py
@@ -1,13 +1,21 @@
 from telethon.sync import TelegramClient
+from telethon import functions, types
 from telethon.tl.functions.messages import AddChatUserRequest
 from telethon.tl.functions.channels import InviteToChannelRequest
-from telethon.errors.rpcerrorlist import UserAlreadyParticipantError, UserNotMutualContactError
-from telethon.tl.types import PeerUser, PeerChat, PeerChannel
+from telethon.errors.rpcerrorlist import UserAlreadyParticipantError, UserNotMutualContactError, UserPrivacyRestrictedError
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel, UserStatusOnline, UserStatusLastWeek, UserStatusRecently, UserStatusOffline
 from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
 import random
 import time
 import os
 import csv
+
+def recently_seen(status):
+    return isinstance(status, UserStatusOnline) or \
+           isinstance(status, UserStatusRecently) or \
+           isinstance(status, UserStatusLastWeek) or \
+           (isinstance(status, UserStatusOffline) and status.was_online > datetime.now(timezone.utc) - timedelta(days=7))
  
 load_dotenv()
 
@@ -40,32 +48,38 @@ if ADD_TO_GROUP or ADD_TO_CHANNEL:
         for raw_user in reader:
             try:
                 user = client.get_entity(PeerUser(int(raw_user[1])))
-                print(f"Attempting to add #{user.id} - {user.username}")
+                status = client(functions.users.GetFullUserRequest(
+                    id=user.id
+                )).user.status
 
-                # Adding to a group
-                if ADD_TO_GROUP:
-                    try:
-                        client(AddChatUserRequest(GROUP_ID, user.id, fwd_limit=10))
-                    except UserAlreadyParticipantError:
-                        print(f"User {user.username} is already a group participant")
-                    else:
-                        print(f"User {user.username} successfully added to the group")
+                if recently_seen(status):
+                    print(f"Attempting to add #{user.id} - {user.username}")
 
-                # Adding to a SuperGroup/Channel
-                if ADD_TO_CHANNEL:
-                    try:            
-                        client(InviteToChannelRequest(CHANNEL, [user.id]))
-                    except UserNotMutualContactError:
-                        print(f"User {user.username} is not a mutual contact")
-                    except UserAlreadyParticipantError:
-                        print(f"User {user.username} is already a channel/supergroup participant")
-                    else:
-                        print(f"User {user.username} successfully added to the channel")
+                    # Adding to a group
+                    if ADD_TO_GROUP:
+                        try:
+                            client(AddChatUserRequest(GROUP_ID, user.id, fwd_limit=10))
+                        except UserAlreadyParticipantError:
+                            print(f"User {user.username} is already a group participant")
+                        else:
+                            print(f"User {user.username} successfully added to the group")
 
-                # Sleep between 30 and 50 seconds
-                sleep_time = 40 + random.randint(-10, 11)
-                print(f"Sleeping for {sleep_time} seconds")
-                time.sleep(sleep_time)    
+                    # Adding to a SuperGroup/Channel
+                    if ADD_TO_CHANNEL:
+                        try:            
+                            client(InviteToChannelRequest(CHANNEL, [user.id]))
+                        except UserNotMutualContactError:
+                            print(f"User {user.username} is not a mutual contact")
+                        except UserAlreadyParticipantError:
+                            print(f"User {user.username} is already a channel/supergroup participant")
+                        else:
+                            print(f"User {user.username} successfully added to the channel")
+
+                    # Sleep between 30 and 50 seconds
+                    sleep_time = 40 + random.randint(-10, 11)
+                    print(f"Sleeping for {sleep_time} seconds")
+                    time.sleep(sleep_time)
+            except UserPrivacyRestrictedError:
+                print(f"PrivacyRestrictedError, skipping {raw_user[0]}")    
             except ValueError:
                 print(f"Value error, skipping {raw_user[0]}")
-
